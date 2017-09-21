f5cea36b1f9a7708ab4293b723eb74adae6016f5
MujunZ <o.6180339@gmail.com>
2017-09-21 19:15:39 -0400

    jump to the post page after addPost submit; clean up comments

--- src/components/App.js
+++ src/components/App.js
@@ -32,26 +32,7 @@ class App extends Component {
       }
       Promise.all(getCmtNumOfPosts).then((posts)=>{this.props.getAllPosts(posts)});
     })
-    // ReadableAPI.getPostById('8xf0y6ziyjabvozdd253nd').then((post) => {
-    //   console.log('getPostById',post);
-    // })
-    // ReadableAPI.getCmtsOfPost('8xf0y6ziyjabvozdd253nd').then((cmt) => {
-    //   console.log('getCmtsOfPost',cmt);
-    // })
-    // ReadableAPI.getCmtsById('894tuq4ut84ut8v4t8wun89g').then((cmt) => {
-    //   console.log('getCmtsById',cmt);
-    // })
   }
-
-  // postElements ( categoryName ) {
-  //   let { title, author, voteScore, timestamp }
-  //         = this.state.posts
-  //           .filter(({ category, deleted }) => category === categoryName && deleted === false);
-  //   return { title, author, voteScore, timestamp }
-  // }
-
-  //openAddPostForm = () => this.setState(() => ({showAddPostForm: true}));
-  //clossAddPostForm = () => this.setState(() => ({showAddPostForm: false}));
   addPost = (e, category) => {
     e.preventDefault();
     let addPostVal = serialize(e.target, { hash: true });
@@ -64,7 +45,7 @@ class App extends Component {
     this.props.addPost(addPostVal);
     this.setState({});
     e.target.reset();
-    window.location = "../"
+    window.location = `../post/${addPostVal.id}`
   }
   render() {
     const { categories=[], posts=[] } = this.props.initState;
